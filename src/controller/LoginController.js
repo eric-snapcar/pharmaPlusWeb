013b5621df801369be62593bac27c9b51f2ceaf6
Eric Hth <eric@snapcar.com>
2018-04-17 00:44:07 +0200

    style.css position absolute ...

--- src/controller/LoginController.js
+++ src/controller/LoginController.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import {Button,ControlGroup,InputGroup, Intent} from "@blueprintjs/core";
 export default class RootController extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {email:null}
+  }
   render() {
     return (
       <div className="loginController">
-        LoginController
+      <ControlGroup  style={{backgroundColor:"white","borderRadius":"3px"}}vertical={true}>
+  <InputGroup  className="pt-large" icon="person" placeholder="Email" value={this.state.email}  placeholder="Email" ref={(input) => { this.emailTextView = input; }} onChange={this.onChangeEmail}/>
+  <InputGroup className="pt-large" icon="lock" placeholder="Password" type="password" value={this.state.password}  placeholder="Password" ref={(input) => { this.passwordTextView = input; }} onChange={this.onChangePassword} onKeyPress={this.keyPressPassword}/>
+  <Button className="pt-large" loading={this.state.loading} onClick={this.login}>Log In</Button>
+</ControlGroup>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RootController from './controller/RootController';
-ReactDOM.render(<RootController />, document.getElementById('root'));
+ReactDOM.render(<RootController />, document.getElementById('app'));
