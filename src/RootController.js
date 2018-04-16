5857e6a60a4757d6083301afaa6e037070f8c7a2
Eric Hth <eric@snapcar.com>
2018-04-16 23:51:30 +0200

    index.js RootController.js

---
+++ src/RootController.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class RootController extends React.Component {
+  render() {
+    return (
+      <div className="rootController">
+        RootController
+      </div>
+    );
+  }
+}
