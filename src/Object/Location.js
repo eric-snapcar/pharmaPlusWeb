31730f263bf4306c0d6f8c1345dbf1fc4217bc89
Eric Hth <eric@snapcar.com>
2018-04-21 13:07:09 +0200

    Location.js this.coordinates ...

--- src/Object/Location.js
+++ src/Object/Location.js
@@ -2,7 +2,11 @@ export default class Location {
   // MARK: class override
     constructor(json){
         if ( json ) {
-            this.coordinates = {lat:json.coordinates.lat,lng:json.coordinates.lng};
+            if(json.coordinates){
+                this.coordinates = {lat:json.coordinates.lat,lng:json.coordinates.lng};
+            }else if(json.coordinate){
+                this.coordinates = {lat:json.coordinate.lat,lng:json.coordinate.lng};
+            }
             this.timestamp = json.timestamp;
         }
     }
