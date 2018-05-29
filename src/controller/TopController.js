734efac92ad26a4d9c724a17a0fc694d96550114
Eric Hth <eric@snapcar.com>
2018-05-30 00:00:36 +0200

    TopController.js created

---
+++ src/controller/TopController.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Button,ControlGroup,InputGroup, Intent} from "@blueprintjs/core";
+export default class TopController extends React.Component {
+  constructor(props){
+    super(props)
+    // this.state = {email:null}
+  }
+  render() {
+    return (
+      <div className="topController">
+        Test
+      </div>
+    );
+  }
+}
