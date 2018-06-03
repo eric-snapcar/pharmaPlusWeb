178393a50ccc5a1e915ef8efac3fb8ec3d7ef6c1
Eric Hth <eric@snapcar.com>
2018-06-03 16:38:49 +0200

    RootController render() this.state.laoding render() LoginController render() this.props.loading

--- src/controller/RootController.js
+++ src/controller/RootController.js
@@ -5,20 +5,34 @@ import FirebaseService from '../service/FirebaseService';
 export default class RootController extends React.Component {
   constructor(props){
     super(props)
-    this.state = {currentUser:null};
+    this.state = {currentUser:null,loading:false};
+
+  }
+  componentDidMount(){
+    this.setState({loading:true})
     FirebaseService.observeAuth((currentUser) => {
-        this.setState({currentUser : currentUser});
+        this.setState({currentUser : currentUser,loading:false});
         if(currentUser ==  null){
             console.log("Not Logged In");
         }
     });
+
   }
   render() {
-    return (
-      <div className="rootController">
-        {this.state.currentUser == null && <LoginController/>}
-        {this.state.currentUser != null && <MainController/>}
-      </div>
-    );
+    if(this.state.loading){
+      return (
+        <div className="rootController">
+            Loading ...
+        </div>
+      );
+    }
+    else {
+        return (
+          <div className="rootController">
+            {this.state.currentUser == null && <LoginController/>}
+            {this.state.currentUser != null && <MainController/>}
+          </div>
+        );
+    }
   }
 }
