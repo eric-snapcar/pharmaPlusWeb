ea2099d2df46be590de298debc41fba701c69716
Eric Hth <eric@snapcar.com>
2018-06-03 17:23:14 +0200

    LoginController this.props.loading

--- src/controller/LoginController.js
+++ src/controller/LoginController.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FirebaseService from '../service/FirebaseService';
-import {Button,ControlGroup,InputGroup, Intent} from "@blueprintjs/core";
+import {Button,ControlGroup,InputGroup, Intent,Spinner} from "@blueprintjs/core";
 export default class RootController extends React.Component {
   constructor(props){
     super(props)
@@ -24,11 +24,14 @@ export default class RootController extends React.Component {
       <div className="loginController">
           <div className="loginViewWrapper">
               <img className="loginControllerSnapcarIcon" src="/static/logo.svg"  alt="" />
-              <ControlGroup  style={{backgroundColor:"white","borderRadius":"3px"}}vertical={true}>
-                  <InputGroup  className="pt-large" icon="person" placeholder="Email" value={this.state.email}  placeholder="Email" ref={(input) => { this.emailTextView = input; }} onChange={this.onChangeEmail}/>
-                  <InputGroup className="pt-large" icon="lock" placeholder="Password" type="password" value={this.state.password}  placeholder="Password" ref={(input) => { this.passwordTextView = input; }} onChange={this.onChangePassword} onKeyPress={this.keyPressPassword}/>
-                  <Button className="pt-large" loading={this.state.loadingSignIn} onClick={() => this.onTapLoginButton()}>Log In</Button>
-              </ControlGroup>
+              {this.props.loadingAuth && <Spinner />}
+              {!this.props.loadingAuth &&
+                  <ControlGroup  style={{backgroundColor:"white","borderRadius":"3px"}}vertical={true}>
+                      <InputGroup  className="pt-large" icon="person" placeholder="Email" value={this.state.email}  placeholder="Email" ref={(input) => { this.emailTextView = input; }} onChange={this.onChangeEmail}/>
+                      <InputGroup className="pt-large" icon="lock" placeholder="Password" type="password" value={this.state.password}  placeholder="Password" ref={(input) => { this.passwordTextView = input; }} onChange={this.onChangePassword} onKeyPress={this.keyPressPassword}/>
+                      <Button className="pt-large" loading={this.state.loadingSignIn} onClick={() => this.onTapLoginButton()}>Log In</Button>
+                  </ControlGroup>
+            }
           </div>
       </div>
     );
