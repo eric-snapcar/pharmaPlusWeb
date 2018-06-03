8ed8c031d5d4f862af0a6241064929ee26a3f476
Eric Hth <eric@snapcar.com>
2018-06-03 16:50:41 +0200

    TopBardController this.logOut() render() import  firebase.auth()

--- src/controller/TopBarController.js
+++ src/controller/TopBarController.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import {Button,ControlGroup,InputGroup, Intent} from "@blueprintjs/core";
+import * as firebase from 'firebase';
 export default class TopBarController extends React.Component {
   constructor(props){
     super(props)
     // this.state = {email:null}
   }
+  logOut(){
+      firebase.auth().signOut().then(function() {
+          console.log("signOut OK");
+      }).catch(function(error) {
+          console.log(error);
+      });
+  }
   render() {
     return (
       <div className="topBarController">
-      <img className="loginControllerSnapcarIcon" src="/static/logo.svg"  alt="" />
+      <img className="loginControllerSnapcarIcon" src="/static/logo.svg" onClick={() => this.logOut()} alt="" />
       </div>
     );
   }
