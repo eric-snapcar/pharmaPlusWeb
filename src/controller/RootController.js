bf3ed5ea9345476fae28fd93e71db5ab7407c020
Eric Hth <eric@snapcar.com>
2018-06-03 16:34:04 +0200

    index.js import firebase .initializeApp(config) RootContorller render()

--- src/controller/RootController.js
+++ src/controller/RootController.js
@@ -2,21 +2,11 @@ import React from 'react';
 import LoginController from './LoginController';
 import MainController from './MainController';
 import FirebaseService from '../service/FirebaseService';
-import * as firebase from 'firebase';
 export default class RootController extends React.Component {
   constructor(props){
     super(props)
-    var config = {
-      apiKey: "AIzaSyA6J9pwWx--tFULFe9zMg7WT2IiVX7gGYs",
-      authDomain: "pharmaplus-782c8.firebaseapp.com",
-      databaseURL: "https://pharmaplus-782c8.firebaseio.com",
-      projectId: "pharmaplus-782c8",
-      storageBucket: "pharmaplus-782c8.appspot.com",
-      messagingSenderId: "191482422002"
-    };
     this.state = {currentUser:null};
-    firebase.initializeApp(config);
-    FirebaseAuthService.observeAuth((currentUser) => {
+    FirebaseService.observeAuth((currentUser) => {
         this.setState({currentUser : currentUser});
         if(currentUser ==  null){
             console.log("Not Logged In");
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RootController from './controller/RootController';
+import * as firebase from 'firebase';
+var config = {
+  apiKey: "AIzaSyA6J9pwWx--tFULFe9zMg7WT2IiVX7gGYs",
+  authDomain: "pharmaplus-782c8.firebaseapp.com",
+  databaseURL: "https://pharmaplus-782c8.firebaseio.com",
+  projectId: "pharmaplus-782c8",
+  storageBucket: "pharmaplus-782c8.appspot.com",
+  messagingSenderId: "191482422002"
+};
+firebase.initializeApp(config);
 ReactDOM.render(<RootController />, document.getElementById('app'));
